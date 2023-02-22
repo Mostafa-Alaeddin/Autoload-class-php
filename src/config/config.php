358f7c6bc46b67d3edd5aa300bfa83f9a369e417
mostafa alaeddin <mostafaalaeddin570@gmail.com>
2023-02-22 12:02:30 +0330

    added realpath || debug

--- src/config/config.php
+++ src/config/config.php
@@ -1,3 +1,5 @@
 <?php
 
-    require_once '../vendor/autoload.php';
+    require_once realpath(
+        __DIR__ . DIRECTORY_SEPARATOR . '..' . DIRECTORY_SEPARATOR . 'vendor' . DIRECTORY_SEPARATOR . 'autoload.php'
+    );
