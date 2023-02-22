223f391fb968bba8a0299b737e70fde8fdb64472
mostafa alaeddin <mostafaalaeddin570@gmail.com>
2023-02-22 12:10:37 +0330

    added PHP_EOL after echo classes

--- src/index.php
+++ src/index.php
@@ -1,8 +1,16 @@
 <?php
-
+//    use class
+    use App\GoodbyeWorld;
     use App\HelloWorld;
 
+//    added config file
     require_once realpath(__DIR__ . DIRECTORY_SEPARATOR . 'config' . DIRECTORY_SEPARATOR . 'config.php');
 
+//    create new class hello world
     $hello_world = new HelloWorld();
-    $hello_world();
+    echo $hello_world() . PHP_EOL;
+
+
+//    create class goodbye world
+    $goodbye_world = new GoodbyeWorld();
+    echo $goodbye_world() . PHP_EOL;
